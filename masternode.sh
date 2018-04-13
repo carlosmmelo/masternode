878fc1b3393558545cff79f2b93e53882ec7cf00
Carlos Melo <carlosmmelo@gmail.com>
2018-04-12 21:23:45 -0400

    Adding output to let user know on every step 3

--- masternode.sh
+++ masternode.sh
@@ -148,7 +148,7 @@ function start_sentinel () {
 
 function set_sentinel_cron () {
     echo "* Creating Cron Job to run Sentinel WatchDog's keep alive every minute"
-    (crontab -l 2>/dev/null; echo "* * * * * export SENTINEL_CONFIG=${XSNCORE_PATH}/sentinel/xsn_sentinel.conf; cd ${XSNCORE_PATH}/sentinel && ${XSNCORE_PATH}/sentinelvenv/bin/python ${XSNCORE_PATH}/sentinel/bin/sentinel.py >/dev/null 2>&1 >> ${XSNCORE_PATH}/sentinel/sentinel-cron.log") | sort - | uniq - | crontab -
+    (crontab -l 2>/dev/null; echo "* * * * * export SENTINEL_CONFIG=${XSNCORE_PATH}/sentinel/xsn_sentinel.conf; cd ${XSNCORE_PATH}/sentinel && ${XSNCORE_PATH}/sentinelvenv/bin/python ${XSNCORE_PATH}/sentinel/bin/sentinel.py >/dev/null 2>&1") | sort - | uniq - | crontab -
 }
 
 function execute_sentinel () {
